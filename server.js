b2d6bb06be1b67f7afce84919f4992a6c6f9d390
Yirun Zhou <nemesisper@gmail.com>
2018-06-10 08:53:11 +0000

    change server.js file again

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
-
-const express = require('express');
-const http = require('http')
-const path = require('path');
-
+    const express = require('express');
 const app = express();
-
-app.use(express.static(path.join(__dirname, 'dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+const path = require('path');
+app.use(express.static('./dist/NCOfficialWeb'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join('./dist/NCOfficialWeb/index.html'));
 });
-
-const port = process.env.PORT || 3000;
-app.set('port', port);
-
-const server = http.createServer(app);
-server.listen(port, () => console.log('running'));
+app.listen(process.env.PORT || 8080);
