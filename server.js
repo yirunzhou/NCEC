aa4163417bb1dffb4cb3ddf215dbafc9233b1be8
Yirun Zhou <nemesisper@gmail.com>
2018-06-10 08:08:35 +0000

    3

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('./dist/NCOfficialWeb'));
-app.get('/*', function(req, res) {
-  res.sendFile(path.join('./dist/NCOfficialWeb/index.html'));
-});
+app.use(express.static(__dirname + '/dist'));
+
 app.listen(process.env.PORT || 8080);
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
 
 console.log('ncec server listening..');
