c0053954227034c475f54afdf65a20f19624ef4c
Yirun Zhou <nemesisper@gmail.com>
2018-06-10 10:46:02 +0000

    change the dirname in server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
     const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/NCOfficialWeb'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/NCOfficialWeb/index.html'));
 });
 app.listen(process.env.PORT || 8080);
