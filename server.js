97277902d04f86d709c0c6b8193c4b32a446407d
Yirun Zhou <nemesisper@gmail.com>
2018-06-10 08:18:25 +0000

    delete depen and use 6's express config

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
+
 const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
-
-app.listen(process.env.PORT || 8080);
-
+app.use(express.static('./dist/NCOfficialWeb'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join('./dist/NCOfficialWeb/index.html'));
 });
+app.listen(process.env.PORT || 8080);
 
 
 console.log('ncec server listening..');
