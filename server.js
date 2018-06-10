5ff3916b40d9f63ed85529b559f8dd3611e56de4
Yirun Zhou <nemesisper@gmail.com>
2018-06-10 08:04:06 +0000

    fix the appname

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('./dist/myappname'));
+app.use(express.static('./dist/NCOfficialWeb'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join('./dist/myappname/index.html'));
+  res.sendFile(path.join('./dist/NCOfficialWeb/index.html'));
 });
 app.listen(process.env.PORT || 8080);
 
