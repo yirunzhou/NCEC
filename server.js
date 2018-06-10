0c1637d152797f9da7e9463f08da5c286749333a
Yirun Zhou <nemesisper@gmail.com>
2018-06-10 10:55:30 +0000

    small fix on title

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-    const express = require('express');
+const express = require('express');
 const app = express();
 const path = require('path');
 app.use(express.static(__dirname + '/dist/NCOfficialWeb'));
